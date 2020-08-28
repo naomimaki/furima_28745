4b44b2857f2fdc961834ef1c53faf5b222248e96
naomimaki <adoresuhakore11@gmail.com>
2020-08-28 16:18:01 +0900

    userモデル・テーブル作成完了

--- db/migrate/20200828071042_devise_create_users.rb
+++ db/migrate/20200828071042_devise_create_users.rb
@@ -4,15 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      # t.string :email,              null: false, default: ""
-      # t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      # t.string   :reset_password_token
-      # t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
       ## Rememberable
-      # t.datetime :remember_created_at
+      t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -31,9 +31,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
+
       t.string   :nickname, null: false
-      t.string   :email, null: false
-      t.string   :password, null: false
       t.string   :family_name, null: false
       t.string   :first_name, null: false
       t.string   :family_name_kana, null: false
