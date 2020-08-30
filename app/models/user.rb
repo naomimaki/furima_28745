5989fef5a3939fd828039def317038a5942f6bb6
naomimaki <adoresuhakore11@gmail.com>
2020-08-30 21:18:22 +0900

    仮置でitems/index Viewを作成・root_pathに設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, :encrypted_password, :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date, presence: true
 end
