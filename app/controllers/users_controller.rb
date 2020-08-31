5b679d1027ebd0afcd7ff1d51a65f11bef9374b8
naomimaki <adoresuhakore11@gmail.com>
2020-08-31 15:42:44 +0900

    ログイン・ログアウト機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
-
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.valid?
+  #     @user.save
+  #     redirect_to root_path
+  #   else
+  #     render 'new'
+  #   end
 end
