fef58a654528fd0b8acb01f42513e23a7f6a61be
naomimaki <adoresuhakore11@gmail.com>
2020-09-06 21:11:55 +0900

    テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,8 +15,14 @@ RSpec.describe Item, type: :model do
       expect(@item).to be_valid
     end
 
-    it "priceが¥300~¥9,999,999の間でなければ登録できない" do
+    it "priceが300円以下であれば登録できない" do
       @item.price = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+    it "priceが9999999円以上であれば登録できない" do
+      @item.price = "10000000"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
@@ -41,6 +47,30 @@ RSpec.describe Item, type: :model do
       @item.category_id = "0"
       @item.valid?
       expect(@item.errors.full_messages).to include("Category Select")
+    end
+
+    it "sales_status_idが0だと登録できない" do
+      @item.sales_status_id = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Sales status Select")
+    end
+
+    it "schedule_delivery_idが0だと登録できない" do
+      @item.schedule_delivery_id = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Schedule delivery Select")
+    end
+
+    it "shipping_fee_status_idが0だと登録できない" do
+      @item.shipping_fee_status_id = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee status Select")
+    end
+
+    it "prefecture_idが0だと登録できない" do
+      @item.prefecture_id = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture Select")
     end
   end
 end
