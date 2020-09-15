00b3ea908ed3df864d8e8f58970fa8047320ecbe
naomimaki <adoresuhakore11@gmail.com>
2020-09-15 15:19:44 +0900

    購入済の場合アドレス直接入力しても購入画面に遷移しない

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,11 +18,14 @@ class OrdersController < ApplicationController
   end
  
   private
-  def move_to_index
-    unless user_signed_in?
+  def move_to_index 
+    @item = Item.find(params[:item_id])
+    # if !user_signed_in? || @item.purchase != nil
+    unless user_signed_in? && @item.purchase == nil
       redirect_to root_path
     end
   end
+
 
   def order_params
     params.require(:order_purchase).permit( :postal_code, :city, :addresses, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
@@ -32,7 +35,7 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      # amount: order_params[:price],  # 商品の値段
+      amount: item_params[:price],  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency:'jpy'                 # 通貨の種類(日本円)
     )
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,16 +3,7 @@ class Order < ApplicationRecord
   
   belongs_to :purchase
   
-  # validates :token, :postal_code, :city, :addresses, :phone_number, presence: true
-
-  # validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
-  # validates :phone_number, length: {maximum: 11} 
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-  # with_options numericality: { other_than: 0, message: 'Select'} do
-  #   validates :prefecture_id
-  # end
-  # とりあえずコメントアウトしておく。
 end
