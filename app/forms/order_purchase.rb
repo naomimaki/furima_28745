6836e75e0dd5098d9dbf98e7bda8c086c8a2e46c
naomimaki <adoresuhakore11@gmail.com>
2020-09-16 15:57:04 +0900

    テストコード訂正

--- app/forms/order_purchase.rb
+++ app/forms/order_purchase.rb
@@ -2,8 +2,6 @@ class OrderPurchase
 
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building_name, :phone_number, :user_id, :item_id, :purchase_id, :token
-
-  # orderテーブルのバリデーション
   validates :token, :postal_code, :city, :addresses, :phone_number, :prefecture_id, presence: true
 
   validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
@@ -12,12 +10,9 @@ class OrderPurchase
   with_options numericality: { other_than: 0, message: 'Select'} do
     validates :prefecture_id
   end
-  # purchaseテーブルのバリデーションを入力→外部キーのみなのでなさげなので空白のままで。
 
   def save
-    # purchaseテーブルの情報を保存
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    # orderの情報を保存しorderという変数に代入トークンを保存したいがカラムには存在しない。
     Order.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building_name: building_name, phone_number: phone_number,purchase_id: purchase.id)
   end
 end
